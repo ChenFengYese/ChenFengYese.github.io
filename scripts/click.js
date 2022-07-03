f59293c7809c668c73e16092ae850ab3dee8a184
ChenFengYese <93316133+ChenFengYese@users.noreply.github.com>
2022-07-03 19:24:35 +0800

    Add files via upload

---
+++ scripts/click.js
@@ -0,0 +1,61 @@
+
+window.addEventListener('load', function () {
+        let body = document.body;
+        let content = ["❤去活出你自己。❤", "❤May all the beauty be blessed❤", "❤愿世上所有美好都能被祝福❤", "❤薪火相传❤", "❤为世上所有的美好而战❤",
+            "❤美德不灭❤", "❤风雨犹祝，山海同欢，是承天地之佑❤", "❤星移斗转，沧海桑田，烟火人间依旧❤", "❤功名在我，百岁千秋，勿忘秉烛夜游❤", "❤今古诸事，激荡中流，宏图待看新秀 ❤",
+            "❤年岁一箸，山海一壶。❤", "❤阅古话今，几度春秋。❤", "❤炊烟喜乐，柴米朝暮。❤", "❤纤云逐月，尘世系舟。❤", "❤世事易变，匪石弗转❤", "❤谁也没有见过爱情，直到有花束抛向自己。❤",
+            "❤与君相别离，不知何日是归期，我如朝露转瞬晞。❤", "❤万商云来，千船继至，百货叠出❤",
+            "❤霞彩换花火,花火知我愿❤", "❤我愿入夜空，夜空自甄明❤", "❤不要放弃❤", "❤醒时恐为梦一场，事事俱忘，何处是吾乡❤", "❤星与你消失之日❤", "❤锦筝叹千年，月下渡忘川❤",
+            "❤曲高未必人不识❤", "❤自有知音和清词❤", "❤花与月与海❤", "❤天空之城❤", "❤难言的微笑❤", "❤隐形的翅膀❤",
+            "❤明天你好❤", "❤regression❤", "❤浮生如梦❤", "❤白鹭归庭❤", "❤一樽还酹江月❤", "❤一梦千宵❤",
+            "❤最后一课❤", "回家的路", "❤乡愁如丝❤", "❤驻足在这星空下❤", "❤年年岁岁花相似❤", "❤天空的音符❤",
+            "❤花之舞❤", "❤彼女は旅に出る❤", "❤所念皆星河❤"
+        ] //自定义内容的数组
+        body.addEventListener('click', function (e) {
+            let x = e.pageX;
+            let y = e.pageY; //当前坐标
+            let randContent = Math.ceil(Math.random() * content.length);
+            let text = new Text(x, y, randContent);
+            let span = document.createElement('span');
+            span.style.color = text.getRandom();
+            text.create(span);
+            setTimeout(function () {
+                text.out(span)
+            }, 1900)
+        })
+
+        function Text(x, y, rand) {
+            this.x = x;
+            this.y = y;
+            this.rand = rand;
+        }
+        Text.prototype.create = function (_this) {
+            let body = document.body;
+            _this.innerHTML = content[this.rand - 1];
+            _this.className = 'text';
+            _this.style.top = this.y - 20 + 'px';
+            _this.style.left = this.x - 50 + 'px';
+            _this.style.animation = 'remove 2s';
+            body.appendChild(_this);
+            let i = 0;
+            setInterval(() => {
+                _this.style.top = this.y - 20 - i + 'px';
+                i++
+            }, 10);
+        };
+        Text.prototype.out = function (_this) {
+            _this.remove()
+        };
+        //设置随机颜色
+        Text.prototype.getRandom = function () {
+            let allType = '0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f'; //16进制颜色
+            let allTypeArr = allType.split(','); //通过','分割为数组
+            let color = '#';
+            for (var i = 0; i < 6; i++) {
+                //随机生成一个0-16的数
+                var random = parseInt(Math.random() * allTypeArr.length);
+                color += allTypeArr[random];
+            }
+            return color; //返回随机生成的颜色
+        }
+    });
---
+++ scripts/index.js
@@ -0,0 +1,174 @@
+function $(s){
+	return document.querySelectorAll(s);
+}
+document.oncontextmenu = function(e){
+	return false;//阻止浏览器的默认弹窗行为
+}
+// event = "music//back.mp3";
+// mv.play(event.target.result);
+var size = 128;//定义的音频数组长度
+
+var box = $('.right')[0];
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+// var line;//渐变色变量
+var height,width;
+var Dots = [];//用于存放点对象数组,点的坐标和颜色信息
+var list = $("#list li");
+
+var mv = new Musicvisualizer({
+	size:size,
+	draw:draw
+});
+
+// 歌曲点击切换
+// for(var i=0;i<list.length;i++){
+// 	list[i].onclick = function(){
+// 		for(var j=0;j<list.length;j++){
+// 			list[j].className = "";
+// 		}
+// 		this.className = "selected";
+// 		mv.play("/media/"+this.title);
+// 		// load("/media/"+this.title);
+// 	};
+// }
+
+// 切换可视化效果类型
+var typeli = $(".type li");
+for(var i=0;i<typeli.length;i++){
+	typeli[i].onclick = function(){
+		for(var j=0;j<typeli.length;j++){
+			typeli[j].className = "";
+		}
+		this.className = "selectedType";
+		draw.type = this.getAttribute("data-type");
+	}
+}
+
+
+
+function getRandom(m,n){
+	return Math.round(Math.random()*(n-m)+m);
+}
+function getDots(){
+	Dots = [];
+	for(var i=0;i<size;i++){
+		var DotX = getRandom(0,width);
+		var DotY = getRandom(0,height);
+		// rgba 增加透明度  最边缘透明度为0
+		var DotColor = "rgba("+getRandom(0,255)+","+getRandom(0,255)+","+getRandom(0,255)+",0)";
+		Dots.push({
+			x:DotX,
+			y:DotY,
+			color:DotColor,
+			cap:0,//柱状上面小方块高度参数
+			dx:getRandom(1,2)
+		});
+	}
+}
+
+/**
+ * [resize 根据窗口大小改变canvas画布大小]
+ * @return {[type]} [description]
+ */
+function resize(){
+	height = box.clientHeight;
+	width = box.clientWidth;
+	canvas.width = width;
+	canvas.height = height;
+
+	// 设置渐变色
+	line = ctx.createLinearGradient(0,0,0,height-10);//线性渐变
+	line.addColorStop(0,"red");
+	line.addColorStop(0.5,"orange");
+	line.addColorStop(1,"green");
+	getDots();
+}
+resize();
+window.onresize = resize;
+
+
+var vol = $("#volume")[0];
+vol.onchange = function(){
+	mv.changeVolumn(this.value/this.max);
+}
+mv.changeVolumn(0.6);//初始化音频大小
+
+function draw(arr){
+
+	ctx.clearRect(0,0,width,height);//每次绘制时，清空上次画布内容
+	ctx.fillStyle = line;
+	var rectWidth = width/size;
+	var cw = rectWidth*0.6;
+	var capHeight = cw > 10?10:cw;//防止上面矩形过高
+	for(var i=0;i<size;i++){
+		var o = Dots[i];
+		if(draw.type == "column"){
+			var rectHeight = arr[i]/256*height;//音频数据最大值256
+			// 绘制矩形条（x,y,width,height）; rectWidth*0.6使矩形之间有间隙
+			ctx.fillRect(rectWidth*i,height-rectHeight,cw,rectHeight);
+			ctx.fillRect(rectWidth*i,height-(o.cap+capHeight),cw,capHeight);
+			o.cap--;
+			if(o.cap<0){
+				o.cap =0;
+			}
+			if(rectHeight>0 && o.cap<rectHeight+40){
+				o.cap = rectHeight+40 > height-capHeight ? height-capHeight:rectHeight+40;
+			}
+		}else if(draw.type == "dot"){
+			ctx.beginPath();//声明，防止各个圆之间连线起来
+			var r = 5+arr[i]/256*(height>width?height:width)/18;//圆的半径 最小10px,并且半径大小会依赖屏幕的宽度大小
+			ctx.arc(o.x,o.y,r,0,Math.PI*2,true);//x,y,半径，起始角度，绘制角度，是否逆时针
+			var round = ctx.createRadialGradient(o.x,o.y,0,o.x,o.y,r);//从圆心到圆最外围
+			round.addColorStop(0,"#fff");
+			round.addColorStop(1,o.color);
+			ctx.fillStyle = round;
+			ctx.fill();
+			// ctx.strokeStyle = round;
+			// ctx.stroke();
+			o.x += o.dx;
+			o.x = o.x > width ? 0 : o.x;
+		}
+
+	}
+}
+draw.type = "column";//默认显示效果类型
+
+$("#add")[0].onclick = function(){
+	swal({
+  title: "",
+  text: "选择本地音乐播放或者网络音乐播放",
+  icon: "warning",
+	buttons: ["网络音乐选择","本地音乐播放"],
+  // buttons: true,
+  dangerMode: true,
+})
+.then((local) => {
+  if (local) {
+    	$("#loadfile")[0].click();
+  } else {
+    openWin('music_choose.html');
+  }
+});
+}
+function openWin(url)
+			 {
+			  var name;                           //网页名称，可为空;
+			  var iWidth=200;                         //弹出窗口的宽度;
+			  var iHeight=200;                        //弹出窗口的高度;
+			  //window.screen.height获得屏幕的高，window.screen.width获得屏幕的宽
+			  var iTop = (window.screen.height-30-iHeight)/2;       //获得窗口的垂直位置;
+			  var iLeft = (window.screen.width-10-iWidth)/2;        //获得窗口的水平位置;
+			  window.open(url,name,'height='+iHeight+',innerHeight='+iHeight+',width='+iWidth+',innerWidth='+iWidth+',top='+iTop+',left='+iLeft+',toolbar=no,menubar=no,scrollbars=auto,resizable=no,location=no,status=no');
+			 }
+$("#loadfile")[0].onchange = function(){
+	var file = this.files[0];
+	var fr = new FileReader();
+
+	fr.onload = function(e){
+		// 重写play方法  这边e.target.result已经是arraybuffer对象类型，不再是ajax路径读入
+		mv.play(e.target.result);
+	}
+	fr.readAsArrayBuffer(file);
+	// $("#loadfile")[0].value = '';
+}
