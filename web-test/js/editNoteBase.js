02deca06f5e0d99b91a1f1db8cd829354116e8b0
ChenFengYese <93316133+ChenFengYese@users.noreply.github.com>
2023-03-07 11:04:15 +0800

    Update editNoteBase.js

--- web-test/js/editNoteBase.js
+++ web-test/js/editNoteBase.js
@@ -29,6 +29,9 @@ function time() {
     hours = date.getHours();
     minutes = date.getMinutes();
     seconds = date.getSeconds();
+    hours =  hours<10?"0"+hours:hours;
+    minutes =  minutes<10?"0"+minutes:minutes;
+    seconds = seconds<10?"0"+seconds:seconds;
     vWeek_s = date.getDay();
     $(".wenzhang_box_content_jieshao_xieti:eq(2)").html(year + "-" + month + "-" + day + " "  + hours + ":" + minutes + ":" + seconds)
 }
