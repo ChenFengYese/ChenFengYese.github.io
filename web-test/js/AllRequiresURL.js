5fa9d84a513b9ee32d52a9fbe867a91916164915
ChenFengYese <93316133+ChenFengYese@users.noreply.github.com>
2023-04-16 19:24:31 +0800

    大修改-chatgpt+人脸识别+动画效果

--- web-test/js/AllRequiresURL.js
+++ web-test/js/AllRequiresURL.js
@@ -1,7 +1,7 @@
 function getURLTest(){
     // return "http://8.130.19.39:8083/";
-    // return "https://maw1b43a8949.ngrok.xiaomiqiu123.top/";
+    return "https://maw1b43a8949.ngrok.xiaomiqiu123.top/";
     // return "http://xsx6owbjz16m.ngrok.xiaomiqiu123.top/"
     // return "http://192.168.174.133:8083/"
-    return "http://localhost:8083/"
+    // return "http://localhost:8083/"
 }
