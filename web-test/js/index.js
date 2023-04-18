972b106a34062b19b44e8eb4ecf18a7587a19388
ChenFengYese <93316133+ChenFengYese@users.noreply.github.com>
2023-04-18 17:21:20 +0800

    服务器错误

--- web-test/js/index.js
+++ web-test/js/index.js
@@ -1,18 +1,39 @@
 
 <!-- Disable the right mouse button -->
-if( $.cookie('FaceTestAuth')==="" || $.cookie('FaceTestAuth')===undefined){
-    //swal确定后在执行后续操作
-    swal({
-        title: "你尚未认证身份",
-        text: "为你的安全以及保护你的隐私,请先进行身份认证",
-        icon: "error",
-        button: "确定",
-    }).then((result) => {
-        if (result.value) {
-            document.location.href = "FaceTest/index.html";
-        }
-    });
-}
+//服务器遭到恶意攻击,现在进入维护状态中,预计两天内维护完成
+swal("服务器遭到恶意攻击", "现在进入维护状态中,预计两天内维护完成", "error")
+
+// if( $.cookie('FaceTestAuth')==="" || $.cookie('FaceTestAuth')===undefined){
+//     //swal确定后在执行后续操作,取消后直接访问
+//     swal({
+//         title: "你尚未认证身份",
+//         text: "为你的安全以及保护你的隐私,请先进行身份认证",
+//         icon: "error",
+//         buttons: true,
+//         button: "确定",
+//         dangerMode: true,
+//     })
+//         .then((result) => {
+//             if (result) {
+//                 document.location.href = "FaceTest/index.html";
+//             } else {
+//                 // swal("Your imaginary file is safe!");
+//             }
+//         }
+//     );
+//
+// }
+// swal({
+//     title: "你尚未认证身份",
+//     text: "为你的安全以及保护你的隐私,请先进行身份认证",
+//     icon: "error",
+//     button: "确定",
+//
+// }).then((result) => {
+//     if (result.value) {
+//         document.location.href = "FaceTest/index.html";
+//     }
+// });
 // swal("数据库暂未运行", "服务器正在全力跑其他项目中,该项目原服务器暂时停止运行.可联系开发者开启临时服务器以试用,临时服务器不具备文件上传功能", "error")
 document.oncontextmenu = function(){
     return false;
